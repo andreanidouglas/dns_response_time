1d809905bda5fd9f9c82ddf07b9e1b7a0620ef90
Douglas Andreani <andreani.dr+github@gmail.com>
2018-04-04 10:31:26 -0300

    new domains and server list

--- dns_time.sh
+++ dns_time.sh
@@ -2,15 +2,13 @@
 
 alias dig='/usr/bin/dig'
 
-domains="google.com uol.com.br estadao.com.br facebook.com amazon.com terra.com.br dev.andreanidr.com fenrir.andreanidr.com"
-dns_servers="8.8.8.8 1.1.1.1 208.67.222.222 9.9.9.9"
-for d in $domains; do
-    for s in $dns_servers; do
-        response_time=$(dig @$s $d | grep -i "Query time:")
-        echo Domain: $d DNS: $s 
-        echo $response_time 
-        echo 
-        sleep 1
-    done
 
+for domain in $(cat domains.txt)
+do
+    for server in $(cat servers.txt)
+    do
+        response_time=$(dig @$server $domain | grep -o "Query time: [0-9]*" | grep -o "[0-9]*")
+        echo $server,$domain,$response_time
+    done
 done
+exit
