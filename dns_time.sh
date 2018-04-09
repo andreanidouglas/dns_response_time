4bac570abb398f88a77cb0761acee043563c0bc3
Douglas Andreani <andreani.dr+github@gmail.com>
2018-04-09 10:31:38 -0300

    Douglas custom server version

--- dns_time.sh
+++ dns_time.sh
@@ -2,13 +2,14 @@
 
 alias dig='/usr/bin/dig'
 
-
+cd /home/pi/dns_response_time
 for domain in $(cat domains.txt)
 do
     for server in $(cat servers.txt)
     do
+        datetime=$(date +"%Y-%m-%d %H:%M:%S")
         response_time=$(dig @$server $domain | grep -o "Query time: [0-9]*" | grep -o "[0-9]*")
-        echo $server,$domain,$response_time
+        echo $datetime,$server,$domain,$response_time
     done
 done
 exit
